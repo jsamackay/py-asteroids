182006a6ddecefc44b922d863d748f70ab70a6be
James Mackay <jmackay13@gmail.com>
2025-03-09 21:19:34 +0000

    Player work added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -7,13 +8,26 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT /2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        for thing in drawable:
+            thing.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
+        tick = clock.tick(60)
+        dt = tick / 1000
 
 
 if __name__ == "__main__":
